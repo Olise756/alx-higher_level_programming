08d38fcf643b9d5d64b79ffe7bf1dc90da226826
Olise756 <isiakponaolise@gmail.com>
2023-01-09 08:17:11 -0800

    m

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+my_list = [1,2,3,4,5]
+
 def element_at(my_list, idx):
     if idx < 0:
         return None
@@ -6,3 +8,4 @@ def element_at(my_list, idx):
         return None
     else:
         print("the element at {} is {} ".format(idx, my_list[idx]))
+element_at(my_list, 3)
