d9609dbec01e346ffbfd82a7ec1bc796a3763517
Olise756 <isiakponaolise@gmail.com>
2023-04-16 16:46:27 -0700

    final updates

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+
 """
-This script changes the name of a State object
+This script updates the name of a State object
 from the database `hbtn_0e_6_usa`.
 """
 
@@ -10,18 +11,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
+
     """
     Updates a State object on the database.
     """
 
     db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3])
-    engine = create_engine(db_uri)
-    Session = sessionmaker(bind=engine)
+    eng = create_engine(db_uri)
+    Session = sessionmaker(bind=eng)
 
     session = Session()
 
-    ari_state = session.query(State).filter(State.id == '2').first()
-    ari_state.name = 'New Mexico'
+    new_name = session.query(State).filter(State.id == '2').first()
+    new_name.name = 'New Mexico'
     session.commit()
     session.close()
--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-This script deletes all State objects
-with a name containing the letter `a`
+This script deletes all objects with a name containing the letter `a`
 from the database `hbtn_0e_6_usa`.
 """
 
@@ -15,15 +14,15 @@ if __name__ == "__main__":
     Deletes State objects on the database.
     """
 
-    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+    db_url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3])
-    engine = create_engine(db_uri)
-    Session = sessionmaker(bind=engine)
+    eng = create_engine(db_url)
+    Session = sessionmaker(bind=eng)
 
     session = Session()
 
-    for instance in session.query(State).filter(State.name.contains('a')):
-        session.delete(instance)
+    for instances in session.query(State).filter(State.name.contains('a')):
+        session.delete(instances)
 
     session.commit()
     session.close()
